c0c3011e9e1c396cda32db0925dbb2a0d8f0d17f
Hyusein Osmanov <osmanovhyus@gmail.com>
2023-10-21 09:18:35 +0300

    Started implementing delete review

--- CinemaCritique/wwwroot/js/review/review.js
+++ CinemaCritique/wwwroot/js/review/review.js
@@ -139,3 +139,4 @@ function fetchUpdatedReviews(movieId) {
         }
     });
 }
+
---
+++ CinemaCritique/wwwroot/js/review/reviewOperations.js
@@ -0,0 +1,20 @@
+﻿function showConfirmationModal() {
+    $(document).ready(function () {
+        $("#removeButton").click(function () {
+            $("#confirmationModal").show();
+        });
+
+        $("#confirmDelete").click(function () {
+            // Execute your delete logic here
+            // For instance, make an AJAX call to delete the review
+
+            $("#confirmationModal").hide();
+        });
+
+        $("#cancelDelete").click(function () {
+            $("#confirmationModal").hide();
+        });
+    });
+
+    document.getElementById("confirmationModal").style.display = "block";
+}
