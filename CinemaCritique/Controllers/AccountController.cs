e20529af22ff0fa18c5f7c05a2d9f01603269ddd
Hyusein Osmanov <osmanovhyus@gmail.com>
2023-11-13 16:40:35 +0200

    Included account controller for custom logout functionality

---
+++ CinemaCritique/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaCritique.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            // Logout logic here
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+    }
+}
