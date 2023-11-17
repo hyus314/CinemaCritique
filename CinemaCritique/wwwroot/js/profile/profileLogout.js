bd20b06fb7830207b44031aac7e1a6b0d351bf0a
Hyusein Osmanov <osmanovhyus@gmail.com>
2023-11-17 22:23:57 +0200

    separeted functionality to js file and made close modal and cancel buttons functional

---
+++ CinemaCritique/wwwroot/js/profile/profileLogout.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    // Show modal on clicking logout button
+    $('#logoutButton').on('click', function () {
+        $('#logoutModal').modal('show');
+    });
+
+    // Close modal on clicking cancel or close button
+    $('#logoutModal button[data-dismiss="modal"]').on('click', function () {
+        $('#logoutModal').modal('hide');
+    });
+
+    // Handle logout confirmation
+    $('#confirmLogout').on('click', function () {
+        // Submit the form when confirmed
+        $('#logoutForm').submit();
+    });
+});
